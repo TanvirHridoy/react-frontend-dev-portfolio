9b9adeb3bfb874addd61f16ea158b343218dede4
TanvirHridoy <anxioushridoy2@gmail.com>
2022-01-26 17:03:59 +0600

    Updates

---
+++ precache-manifest.a78e8ec217e4f0905fde31230e6a4b72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856b328d01d069ef6bd6a0091591f77a",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/index.html"
+  },
+  {
+    "revision": "bafab2b971c10acd8662",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e18081c23520e47b9631",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "bafab2b971c10acd8662",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/static/js/2.aaf472b0.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/static/js/2.aaf472b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18081c23520e47b9631",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/static/js/main.37e8f8f8.chunk.js"
+  },
+  {
+    "revision": "e4d8206e6f25d9b19104",
+    "url": "/TanvirHridoy/TanvirHridoy.github.io/static/js/runtime-main.f07efbdd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.617f0b8a52a62e7bfd104d6d39b9ce6e.js"
+  "/TanvirHridoy/TanvirHridoy.github.io/precache-manifest.a78e8ec217e4f0905fde31230e6a4b72.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TanvirHridoy/TanvirHridoy.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
